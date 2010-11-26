8a8b418a164a496c890cba0192caef5daeb2fd9f
Fubra <Fubra@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-26 12:35:14 +0000

    1.3.8 commit
    git-svn-id: http://plugins.svn.wordpress.org/wp-survey-and-quiz-tool/trunk@315797 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/site/survey.php
+++ includes/site/survey.php
@@ -128,8 +128,8 @@ function wpsqt_site_survey_fetch_questions(){
 		
 	$questions = $wpdb->get_results( $wpdb->prepare('SELECT * FROM `'.WPSQT_SURVEY_QUESTIONS_TABLE.'` WHERE surveyid = %d AND type = %s AND sectionid = %d ORDER BY id ASC LIMIT 0,%d',
 													array($surveyId,$section['type'],$section['id'],$section['number'] )), ARRAY_A );
-	var_dump(  $wpdb->prepare('SELECT * FROM `'.WPSQT_SURVEY_QUESTIONS_TABLE.'` WHERE surveyid = %d AND type = %s AND sectionid = %d ORDER BY id ASC LIMIT 0,%d',
-													array($surveyId,$section['type'],$section['id'],$section['number'] ) ) );
+	$wpdb->prepare('SELECT * FROM `'.WPSQT_SURVEY_QUESTIONS_TABLE.'` WHERE surveyid = %d AND type = %s AND sectionid = %d ORDER BY id ASC LIMIT 0,%d',
+													array($surveyId,$section['type'],$section['id'],$section['number'] ) );
 	if ( $section['type'] == 'multiple' ){		
 		for ( $i = 0; $i < sizeof($questions); $i++){
 			$questions[$i]['answers'] = $wpdb->get_results('SELECT * FROM `'.WPSQT_SURVEY_ANSWERS_TABLE.'` WHERE questionid = '. $questions[$i]['id'] , ARRAY_A );
