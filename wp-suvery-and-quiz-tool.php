9bb9c82e375497d514822b173ae692d6d3e42222
Fubra <Fubra@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-06 15:54:20 +0000

    1.3.16 commit
    git-svn-id: http://plugins.svn.wordpress.org/wp-survey-and-quiz-tool/trunk@319685 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-suvery-and-quiz-tool.php
+++ wp-suvery-and-quiz-tool.php
@@ -6,7 +6,7 @@ Plugin URI: http://catn.com/2010/10/04/wp-survey-and-quiz-tool/
 Description: A plugin to allow wordpress owners to create their own web based quizes.
 Author: Fubra Limited
 Author URI: http://www.catn.com
-Version: 1.3.15
+Version: 1.3.16
 */
 
 //TODO leverage media overlay for questions
@@ -61,7 +61,7 @@ define( 'WPSQT_PAGE_CATN'            , 'wpsqt-catn' );
 define( 'WPSQT_URL_MAIN'             , get_bloginfo('url').'/wp-admin/admin.php?page='.WPSQT_PAGE_MAIN );
 
 define( 'WPSQT_CONTACT_EMAIL'        , 'support@catn.com' );
-define( 'WPSQT_VERSION'              , '1.3.15' );
+define( 'WPSQT_VERSION'              , '1.3.16' );
 define( 'WPSQT_DIR'                  , dirname(__FILE__) );
 
 // start a session
@@ -215,7 +215,7 @@ function wpsqt_main_install(){
 				  `results` text NOT NULL,
 				  `ipaddress` varchar(255) NOT NULL,
 				  `user_agent` varchar(255) NOT NULL,
-				3  PRIMARY KEY (`id`)
+				  PRIMARY KEY (`id`)
 				) ENGINE=MyISAM  DEFAULT CHARSET=latin1;");
 	
 	$wpdb->query("CREATE TABLE IF NOT EXISTS `".WPSQT_FORM_TABLE."` (
@@ -636,7 +636,17 @@ function wpsqt_main_db_upgrade(){
 	$wpdb->query("ALTER TABLE `".WPSQT_QUIZ_TABLE."` ADD `display_review` VARCHAR( 3 ) NOT NULL DEFAULT 'no'");
 	
 	// REMEMBER TO UPDATE THE INSTALL SQL
-	
+	// 1.3.16
+	$wpdb->query("CREATE TABLE IF NOT EXISTS `".WPSQT_SURVEY_SINGLE_TABLE."` (
+				  `id` int(11) NOT NULL AUTO_INCREMENT,
+				  `surveyid` int(11) NOT NULL,
+				  `person` text NOT NULL,
+				  `name` varchar(255) NOT NULL,
+				  `results` text NOT NULL,
+				  `ipaddress` varchar(255) NOT NULL,
+				  `user_agent` varchar(255) NOT NULL,
+				  PRIMARY KEY (`id`)
+				) ENGINE=MyISAM  DEFAULT CHARSET=latin1;");
 	return;
 }
 
