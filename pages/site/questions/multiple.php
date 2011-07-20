a88841f2fa77b8e5c459ef4b61d9247f335f030d
Ollie Armstrong <olliea95@gmail.com>
2011-07-20 15:18:37 +0100

    Fix types

--- pages/site/questions/multiple.php
+++ pages/site/questions/multiple.php
@@ -1,7 +1,7 @@
 			<ul>
 			<?php foreach ( $question['answers'] as $answerKey => $answer ){ ?>
 				<li>
-					<input type="<?php echo ($question['type'] == 'Single' || $question['type'] == 'Multiple Choice' ) ? 'radio' : 'checkbox'; ?>" name="answers[<?php echo $questionKey; ?>][]" value="<?php echo $answerKey; ?>" id="answer_<?php echo $question['id']; ?>_<?php echo $answerKey;?>" <?php if ( (isset($answer['default']) && $answer['default'] == 'yes') || in_array($answerKey, $givenAnswer)) {  ?> checked="checked" <?php } ?> /> <label for="answer_<?php echo $question['id']; ?>_<?php echo $answerKey;?>"><?php echo htmlentities( $answer['text'] ); ?></label> 
+					<input type="<?php echo ($question['type'] == 'Single' ) ? 'radio' : 'checkbox'; ?>" name="answers[<?php echo $questionKey; ?>][]" value="<?php echo $answerKey; ?>" id="answer_<?php echo $question['id']; ?>_<?php echo $answerKey;?>" <?php if ( (isset($answer['default']) && $answer['default'] == 'yes') || in_array($answerKey, $givenAnswer)) {  ?> checked="checked" <?php } ?> /> <label for="answer_<?php echo $question['id']; ?>_<?php echo $answerKey;?>"><?php echo htmlentities( $answer['text'] ); ?></label> 
 				</li>
 			<?php } 
 				if (    $question['type'] == 'Multiple Choice' 
@@ -9,7 +9,7 @@
 					 && $question['include_other'] == 'yes' ){					
 				?>
 				<li>
-					<input type="radio" name="answers[<?php echo $questionKey; ?>]" value="0" id="answer_<?php echo $question['id']; ?>_other"> <label for="answer_<?php echo $question['id']; ?>_other">Other</label> <input type="text" name="other[<?php echo $questionKey; ?>]" value="" />
+					<input type="checkbox" name="answers[<?php echo $questionKey; ?>]" value="0" id="answer_<?php echo $question['id']; ?>_other"> <label for="answer_<?php echo $question['id']; ?>_other">Other</label> <input type="text" name="other[<?php echo $questionKey; ?>]" value="" />
 				</li>
 				<?php } ?>
 			</ul>
