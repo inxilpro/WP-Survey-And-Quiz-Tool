f222c28d537cb5e3e320083ba54b2972f883d261
Ollie Armstrong <olliea95@gmail.com>
2011-08-02 22:54:37 +0800

    Update versions

--- wp-survey-and-quiz-tool.php
+++ wp-survey-and-quiz-tool.php
@@ -50,7 +50,7 @@ define( 'WPSQT_TABLE_SURVEY_CACHE'   , $wpdb->get_blog_prefix().'wpsqt_survey_ca
 define( 'WPSQT_URL_MAIN'             , admin_url('admin.php?page='.WPSQT_PAGE_MAIN) );
 define( 'WPSQT_URL_MAINENTANCE'      , admin_url('admin.php?page='.WPSQT_PAGE_MAINTENANCE) );
 define( 'WPSQT_CONTACT_EMAIL'        , 'support@catn.com' );
-define( 'WPSQT_VERSION'              , '2.2.3' );
+define( 'WPSQT_VERSION'              , '2.3' );
 define( 'WPSQT_DIR'                  , dirname(__FILE__).'/' );
 define( 'WPSQT_FILE'     , __FILE__ );
 
