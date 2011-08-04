346988ee1825e7f3ccc1f8cb182b2f2b6b3fce64
Ollie Armstrong <olliea95@gmail.com>
2011-08-04 09:48:10 +0100

    Fix poll not displaying & admin fixes

--- lib/Wpsqt/Page/Main/Questiondelete/Poll.php
+++ lib/Wpsqt/Page/Main/Questiondelete/Poll.php
@@ -2,12 +2,12 @@
 require_once WPSQT_DIR.'lib/Wpsqt/Page/Main/Questiondelete.php';
 
 	/**
-	 * Deletes quiz questions.
+	 * Deletes poll questions.
 	 * 
-	 * @author Iain Cambridge
+	 * @author Ollie Armstrong
 	 * @copyright Fubra Limited 2010-2011, all rights reserved
   	 * @license http://www.gnu.org/licenses/gpl.html GPL v3 
   	 * @package WPSQT
 	 */
 
-class Wpsqt_Page_Main_Questiondelete_Quiz extends Wpsqt_Page_Main_Questiondelete { }
+class Wpsqt_Page_Main_Questiondelete_Poll extends Wpsqt_Page_Main_Questiondelete { }
