ebac1115555a7fc228542648f39f5422ea845b12
Ollie Armstrong <sexii.ollie@gmail.com>
2011-10-25 11:01:56 +0100

    Fix likert results

--- pages/admin/surveys/result.total.php
+++ pages/admin/surveys/result.total.php
@@ -2,22 +2,22 @@
 
 	<div id="icon-tools" class="icon32"></div>
 	<h2>WP Survey And Quiz Tool - Survey Results</h2>
-		
+
 	<?php require WPSQT_DIR.'pages/admin/misc/navbar.php'; ?>
-	
+
 	<?php if ( $sections == false ) { ?>
-	
+
 		<p>There are no results for this survey yet.</p>
-	
+
 	<?php } else { ?>
-	
+
 		<?php foreach ( $sections as $sectionKey => $secton ){
 				foreach ( $secton['questions'] as $questonKey => $question ) {
-			?> 
+			?>
 				<h3><?php echo $question['name']; ?></h3>
-				
+
 				<?php if ( $question['type'] == "Multiple Choice" ||
-						   $question['type'] == "Dropdown" ) { 
+						   $question['type'] == "Dropdown" ) {
 							$googleChartUrl = 'http://chart.apis.google.com/chart?chs=293x185&cht=p';
 							$valueArray    = array();
 							$nameArray     = array();
@@ -25,41 +25,59 @@
 						   		$nameArray[] = $answer['text'];
 								$valueArray[] = $answer['count'];
 						   }
-			 
+
 							$googleChartUrl .= '&chd=t:'.implode(',', $valueArray);
 							$googleChartUrl .= '&chdl='.implode('|',$nameArray);
 							$googleChartUrl .= '&chtt='.$question['name'];
 							?>
-			 
+
 							<img src="<?php echo $googleChartUrl; ?>" alt="<?php echo $question['name']; ?>" />
-					<?php } else if ($question['type'] == "Free Text") { 
-					
+					<?php } else if ($question['type'] == "Free Text") {
+
 							$i = 1; // Variable used to count answers - used later
-							
+
 							?> <em>All answers for this question</em> <?php
-							
+
 							foreach($uncachedresults as $uresult) {
 								$usection = unserialize($uresult['sections']);
-								
+
 								foreach($usection as $result) {
-									
+
 									foreach($result['answers'] as $uanswerkey => $uanswer) {
 										if($uanswerkey == $questonKey && in_array($uanswerkey, $freetextq)) {
 											echo '<p>'.$i.') '.$uanswer['given'][0].'</p>';
 											$i++;
 										}
-										
+
 									}
 								}
-								
+
 							}
+						  } else if ($question['type'] == "Likert") {
+								$googleChartUrl = 'http://chart.apis.google.com/chart?chs=350x250&cht=bvs';
+								$valueArray    = array();
+								$nameArray     = array();
+								$maxValue = 0;
+								foreach ( $question['answers'] as $key => $answer ) {
+									$nameArray[] = $key;
+									$valueArray[] = $answer['count'];
+									// Gets the maximum value
+									if ($answer['count'] > $maxValue)
+										$maxValue = $answer['count'];
+								}
+								$googleChartUrl .= '&chm=N,000000,0,,10|N,000000,1,,10|N,000000,2,,10'; // Adds the count above bars
+								$googleChartUrl .= '&chds=0,'.(++$maxValue); // Sets scaling to a little bit more than max value
+								$googleChartUrl .= '&chxt=x'; // Sets labelling to x-axis only
+								$googleChartUrl .= '&chd=t:'.implode(',', $valueArray); // Chart data
+								$googleChartUrl .= '&chtt='.$question['name']; // Chart title
+								?><img src="<?php echo $googleChartUrl; ?>" alt="<?php echo $question['name']; ?>" /><?php
 						  } else {
 								echo 'Something went really wrong, please report this bug to the forum. Here\'s a var dump which might make you feel better.<pre>'; var_dump($question); echo '</pre>';
-						  } ?>	
-				
+						  } ?>
+
 		<?php }
 		}
-	} ?>	
-	
-</div>	
+	} ?>
+
+</div>
 <?php require_once WPSQT_DIR.'/pages/admin/shared/image.php'; ?>
