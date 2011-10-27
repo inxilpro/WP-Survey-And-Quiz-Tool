fd18de11e8e42bcc0394e406b4e59868ed7de8ec
Ollie Armstrong <sexii.ollie@gmail.com>
2011-10-27 14:25:48 +0100

    Fix delete survey results with dropdown

--- lib/Wpsqt/Page/Main/Resultsdelete/Survey.php
+++ lib/Wpsqt/Page/Main/Resultsdelete/Survey.php
@@ -24,7 +24,7 @@ class Wpsqt_Page_Main_Resultsdelete_Survey extends Wpsqt_Page_Main_Resultsdelete
 			foreach ($answers as $key => &$answer) {
 				if ($cachedQuestions2[$key]['type'] == 'Likert')
 					$cachedQuestions2[$key]['answers'][$answer['given']]['count']--;
-				if ($cachedQuestions2[$key]['type'] == 'Multiple Choice') {
+				if ($cachedQuestions2[$key]['type'] == 'Multiple Choice' || $cachedQuestions2[$key]['type'] == 'Dropdown') {
 					$answerGiven = (int)$answer['given'];
 					$cachedQuestions2[$key]['answers'][$answerGiven]['count']--;
 				}
