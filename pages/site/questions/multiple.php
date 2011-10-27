b1b7c1e45a86c0c5517dfeb4da6c5b24e76ab6ec
Ollie Armstrong <sexii.ollie@gmail.com>
2011-10-27 16:09:47 +0100

    Changed class names of several elements

--- pages/site/questions/multiple.php
+++ pages/site/questions/multiple.php
@@ -1,4 +1,4 @@
-			<ul style="list-style: none; margin-left: 0;">
+			<ul class="wpsqt_multiple_question">
 			<?php foreach ( $question['answers'] as $answerKey => $answer ){ ?>
 				<li>
 					<input type="<?php echo ($question['type'] == 'Single' ) ? 'radio' : 'checkbox'; ?>" name="answers[<?php echo $questionKey; ?>][]" value="<?php echo $answerKey; ?>" id="answer_<?php echo $question['id']; ?>_<?php echo $answerKey;?>" <?php if ( (isset($answer['default']) && $answer['default'] == 'yes') || in_array($answerKey, $givenAnswer)) {  ?> checked="checked" <?php } ?> /> <label for="answer_<?php echo $question['id']; ?>_<?php echo $answerKey;?>"><?php echo esc_html( $answer['text'] ); ?></label> 
